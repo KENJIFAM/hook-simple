8014ef06ac0b3825638c2f630d3c87f35f5e305c
Khoa Pham <kenjifam@gmail.com>
2019-01-11 23:46:06 +0200

    resource, setResource

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import ResourceList from './ResourceList';
+
+interface State {
+  resource: string;
+}
 
 const App = () => {
-  return <div>App</div>;
-}
+  const [resource, setResource] = React.useState('posts');
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Post</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
